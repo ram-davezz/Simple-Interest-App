dbea2b6d2739606f38f69bf143767da89f9c7f01
sreer <sreer@LAPTOP-PHAA3V2J>
2022-02-24 17:05:33 +0530

    commit

---
+++ SimpleInterestApp/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SimpleInterestApp
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+         
+        }
+
+
+
+        private void Calculate(object sender, EventArgs e)
+        {
+            double Total = (Convert.ToDouble(txtPrincipal_amt.Text) * Convert.ToDouble(txtMonths.Text) * Convert.ToDouble(txtInterest.Text)) / 100;
+            txtTotal_amt.Text = Total.ToString();
+
+        }
+
+        private void Clear(object sender, EventArgs e)
+        {
+            txtPrincipal_amt.Text = "";
+            txtInterest.Text = "";
+            txtMonths.Text = "";
+            txtTotal_amt.Text = "";
+        }
+
+
+    }
+}
